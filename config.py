95bce043c0caa9d1b7e756087528abb1cdf08067
Heri Rusmanto <retphern@gmail.com>
2017-12-12 05:06:21 +0700

    Change config back to mandrill

--- config.py
+++ config.py
@@ -11,13 +11,13 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BCRYPT_LOG_ROUNDS = 15
-    MAIL_SERVER = 'smtp.mandrillapp.com'
+    MAIL_SERVER = 'smtp.mandrill.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = False
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ['MAIL_USERNAME']
     MAIL_PASSWORD = os.environ['MAIL_PASSWORD']
-    MAIL_DEFAULT_SENDER = 'hvedaid@gmail.com.com'
+    MAIL_DEFAULT_SENDER = 'hvedaid@gmail.com'
 
 
 class ProductionConfig(Config):
